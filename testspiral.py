9e4df152110ac392d567e381d80680faf660b476
Rithwik Babu <rithwikbabu13@gmail.com>
2021-08-07 13:08:21 -0500

    Cleaned Code

--- testspiral.py
+++ testspiral.py
@@ -2,90 +2,56 @@ from turtle import *
 import turtle
 import random
 
-tot = 0
-points_x = []
-points_y = []
-
-angles = []
-i = 0
-while i < 360:
-    angles.append(i)
-    i += 60
+points_x, points_y = [],[]
 
 ts = turtle.getscreen()
 turtle.screensize(canvwidth=5000, canvheight=5000,
                   bg="white")
-
 ts.tracer(False)
-systurtle = turtle.Turtle()
-systurtle.pencolor("red")
-def gen_system(xpos, ypos, times):
-    for _ in range(times):
-        systurtle.penup()
-        systurtle.setpos(xpos, ypos)
+
+def gen_system(xpos, ypos, count):
+    s = turtle.Turtle()
+    for _ in range(count):
+        s.penup()
+        s.setpos(xpos, ypos)
+        s.right(random.randrange(0, 360))
         if random.randrange(0, 5) == 3:
-            systurtle.pencolor("blue")
-            systurtle.forward(random.randrange(101, 700))
+            s.pencolor("blue")
+            s.forward(random.randrange(101, 700))
         else:
-            systurtle.pencolor("red")
-            systurtle.forward(random.randrange(10, 100))
-        print(systurtle.xcor(), systurtle.ycor())
-        points_x.append(systurtle.xcor())
-        points_y.append(systurtle.ycor())
-        systurtle.pendown()
-        systurtle.right(random.randrange(0, 360))
-        systurtle.circle(1)
+            s.pencolor("red")
+            s.forward(random.randrange(10, 100))
+        s.pendown()
+        s.circle(1)
+        points_x.append(s.xcor())
+        points_y.append(s.ycor())
 
-for x in angles:
+def gen_galaxy_line(turt, start, stop, systems, spawn_chance, multiplier):
+    if start > stop:
+        gen_galaxy_line(turt, start - 1, stop, systems, spawn_chance, multiplier)
+        turt.forward((random.randrange(1, 9)/10+start)*multiplier)
+        turt.right(random.randrange(3, 9))
+        if random.randrange(0, spawn_chance) == 0:
+            gen_system(int(turt.xcor()), int(turt.ycor()), systems)
+        
+        return turt
+
+for angle in range(0, 360, 60):
     a = turtle.Turtle()
-    a.right(x)
-    dev = random.randrange(20, 40) 
-    atotang = 0
-    for i in range(dev):
-        a.forward(random.randrange(1, 9)/10+i)
-        tempang = random.randrange(3, 9)
-        a.right(tempang)
-        atotang += tempang
-        if random.randrange(0, 5) == 3:
-            gen_system(int(a.xcor()), int(a.ycor()), 80)
-            tot+=80
+    a.right(angle)
+    first_section_length = random.randrange(30, 60)
 
-    b = turtle.Turtle()
-    b.penup()
-    b.setpos(int(a.xcor()), int(a.ycor()))
-    b.pendown()
-    b.right(x + atotang + 10)
-    btotang = 0
-    for i in range(dev, 50):
-        a.forward(random.randrange(1, 9)/10+2*i)
-        a.right(random.randrange(3, 9))
-        b.forward(random.randrange(1, 9)/10+2*i)
-        tempang = random.randrange(3, 9)
-        b.right(tempang)
-        btotang += tempang
-        if random.randrange(0, 3) == 2:
-                gen_system(int(b.xcor()), int(b.ycor()), 40)
-                tot+=40
-        if random.randrange(0, 3) == 2:
-                gen_system(int(a.xcor()), int(a.ycor()), 40)
-                tot+=40
-        if random.randrange(0, 25) == 5:
-            c = turtle.Turtle()
-            c.penup()
-            c.setpos(int(b.xcor()), int(b.ycor()))
-            c.pendown()
-            c.right(x + btotang + atotang + 20)
-            for ij in range(10):
-                c.forward(random.randrange(1, 9)/10+4*i)
-                tempang = random.randrange(3, 9)
-                c.right(tempang)
-                gen_system(int(c.xcor()), int(c.ycor()), 80)
-                tot+=80
-print(tot)
+    a = gen_galaxy_line(a, first_section_length, 0, 80, 5, 1)
+    b = a.clone()
+    b.right(10)
+
+    gen_galaxy_line(a, 100, first_section_length, 80, 2, 2)
+    gen_galaxy_line(b, 100, first_section_length, 80, 2, 2)
+    
+print("Total Systems: ", len(points_x))
 res = [points_x, points_y]
 
 import csv
-
 with open("points.csv","w+") as my_csv:
     csvWriter = csv.writer(my_csv,delimiter=',')
     csvWriter.writerows(res)
