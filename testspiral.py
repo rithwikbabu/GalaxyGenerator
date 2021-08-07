3910f54fff0c184e9c4760bf7124aa8baa26f809
Rithwik Babu <rithwikbabu13@gmail.com>
2021-08-07 13:20:04 -0500

    Added Comments

--- testspiral.py
+++ testspiral.py
@@ -4,35 +4,37 @@ import random
 
 points_x, points_y = [],[]
 
+# Screen Config
 ts = turtle.getscreen()
 turtle.screensize(canvwidth=5000, canvheight=5000,
                   bg="white")
-ts.tracer(False)
+ts.tracer(False) #removing turtle animations
 
+# Generates Solar Systems
 def gen_system(xpos, ypos, count):
-    s = turtle.Turtle()
-    for _ in range(count):
+    s = turtle.Turtle() #init turtle
+    for _ in range(count): #loop thru wanted solar system
         s.penup()
-        s.setpos(xpos, ypos)
-        s.right(random.randrange(0, 360))
-        if random.randrange(0, 5) == 3:
+        s.setpos(xpos, ypos) #move turtle to the line
+        s.right(random.randrange(0, 360)) #spin turtle in a random direction
+        if random.randrange(0, 5) == 3: #chance to spawn far
             s.pencolor("blue")
-            s.forward(random.randrange(101, 700))
-        else:
+            s.forward(random.randrange(101, 700)) #random distance
+        else: #chance to spawn close
             s.pencolor("red")
-            s.forward(random.randrange(10, 100))
+            s.forward(random.randrange(10, 100)) #random distance
         s.pendown()
-        s.circle(1)
+        s.circle(1) #spawn system
         points_x.append(s.xcor())
         points_y.append(s.ycor())
 
-def gen_galaxy_line(turt, start, stop, systems, spawn_chance, multiplier):
+# Generates Galaxy Lines
+def gen_galaxy_line(turt, start, stop, systems, multiplier):
     if start > stop:
-        gen_galaxy_line(turt, start - 1, stop, systems, spawn_chance, multiplier)
+        gen_galaxy_line(turt, start - 1, stop, systems, multiplier)
         turt.forward((random.randrange(1, 9)/10+start)*multiplier)
         turt.right(random.randrange(3, 9))
-        if random.randrange(0, spawn_chance) == 0:
-            gen_system(int(turt.xcor()), int(turt.ycor()), systems)
+        gen_system(int(turt.xcor()), int(turt.ycor()), systems)
         
         return turt
 
@@ -41,12 +43,12 @@ for angle in range(0, 360, 60):
     a.right(angle)
     first_section_length = random.randrange(30, 60)
 
-    a = gen_galaxy_line(a, first_section_length, 0, 80, 5, 1)
+    a = gen_galaxy_line(a, first_section_length, 0, random.randrange(20, 40), 1)
     b = a.clone()
     b.right(10)
 
-    gen_galaxy_line(a, 100, first_section_length, 80, 2, 2)
-    gen_galaxy_line(b, 100, first_section_length, 80, 2, 2)
+    gen_galaxy_line(a, 100, first_section_length, random.randrange(20, 80), 2)
+    gen_galaxy_line(b, 100, first_section_length, random.randrange(20, 80), 2)
     
 print("Total Systems: ", len(points_x))
 res = [points_x, points_y]
